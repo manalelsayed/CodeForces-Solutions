ea93d0e1095d291191e9c5a30cd0e003563ccd53
Manal ELSAYED <manal.elsayed@intalio.com>
2024-02-07 16:05:56 +0200

    CF405-D2-A Gravity Flip

---
+++ C# Solutioms/CF405-D2-A.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.C__Solutioms
+{
+    internal class CF405_D2_A
+    {
+        public static void sol()
+        {
+
+            //https://codeforces.com/contest/405/problem/A Gravity Flip
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            Array.Sort(arr);
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+
+
+        }
+    }
+}
