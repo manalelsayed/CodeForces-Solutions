2941574c6eb4a5f9e00595cc1200029dea25f177
Manal ELSAYED <manal.elsayed@intalio.com>
2024-02-07 15:15:02 +0200

    CF263_D2_A Beautiful Matrix

---
+++ C# Solutioms/CF263-D2-A.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.C__Solutioms
+{
+    internal class CF263_D2_A
+    {
+        public static void sol()
+        {
+            //https://codeforces.com/contest/263/problem/A  A. Beautiful Matrix
+            //    <-I->
+            //    - - -       
+            //0 0 0 1 0 | 
+            //0 0 0 0 0 | J
+            //0 0 0 0 0 |
+            //0 0 0 0 0
+            //0 0 0 0 0
+
+            //steps =   -J -I 4-0-3
+            //0,3
+            int[,] matrix = new int[5, 5];
+            int i1 = 0;
+            int j1 = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                var iRow = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = iRow[j];
+                    if (iRow[j] == 1)
+                    {
+
+                        i1 = i;
+
+
+                        j1 = j;
+
+
+                    }
+                }
+            }
+            Console.WriteLine(Math.Abs(2 - i1) + Math.Abs(2 - j1));
+
+
+        }
+    }
+}
