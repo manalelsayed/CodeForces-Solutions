09ea31a6b65f45dbb3a4e96af98f35afbeed4618
Manal ELSAYED <manal.elsayed@intalio.com>
2024-02-09 23:16:39 +0200

    CF236_D2_A Boy Or Girl

---
+++ C# Solutioms/CF236-D2-A.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.C__Solutioms
+{
+    internal class CF236_D2_A
+    {
+        //https://codeforces.com/contest/236/problem/A Boy Or Girl
+        public static void sol()
+        {
+            string s = Console.ReadLine();
+            HashSet<char> distinctChars = new HashSet<char>(s);
+            Console.WriteLine((distinctChars.Count % 2 != 0) ? "IGNORE HIM!" : "CHAT WITH HER!");
+
+        }
+
+    }
+}
