2b3c4f3dbc2b875b14f272ea369269ada46a4d3a
Manal ELSAYED <manal.elsayed@intalio.com>
2024-02-10 01:08:02 +0200

    CF344-D2-A Magnets

---
+++ C# Solutioms/CF344-D2-A.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.C__Solutioms
+{
+    internal class CF344_D2_A
+    {
+        //Magnets https://codeforces.com/contest/344/problem/A
+        public void sol()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int numOfGroubs = 0;
+            string lastChar = String.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                string CurrentChar = Console.ReadLine();
+                if (CurrentChar != lastChar) { numOfGroubs++; }
+                lastChar = CurrentChar;
+            }
+            Console.WriteLine(numOfGroubs.ToString());
+        }
+    }
+}
