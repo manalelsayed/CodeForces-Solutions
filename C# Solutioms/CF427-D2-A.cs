7f5cb231d7cbaab592a0d14b2cc07a95113f4e8b
Manal ELSAYED <manal.elsayed@intalio.com>
2024-02-11 21:08:21 +0200

    CF427-D2-A Police Recruits

---
+++ C# Solutioms/CF427-D2-A.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.C__Solutioms
+{
+    internal class CF427_D2_A
+    {
+        //https://codeforces.com/contest/427/problem/A Police Recruits
+        public void sol()
+        {
+            int numberOfEvents = int.Parse(Console.ReadLine());
+            int[] arr = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int numOfAvailableOfficers = 0;
+            int unSolvedCrimes = 0;
+
+
+            for (int i = 0; i < numberOfEvents; i++)
+            {
+
+                if (arr[i] > 0)
+                {
+                    numOfAvailableOfficers += arr[i];
+                }
+                else
+                {
+                    if (numOfAvailableOfficers > 0)
+                    {
+                        numOfAvailableOfficers--;
+                    }
+                    else
+                    {
+                        unSolvedCrimes++;
+                    }
+                }
+
+            }
+            Console.WriteLine(unSolvedCrimes.ToString());
+        }
+    }
+}
