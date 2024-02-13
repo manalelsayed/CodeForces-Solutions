066ed861b134e935ef7c4a5e674ca6257b00c049
Manal ELSAYED <manal.elsayed@intalio.com>
2024-02-13 22:27:53 +0200

    CF431-D2-A  Black Square

---
+++ C# Solutioms/CF431-D2-A.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.C__Solutioms
+{
+    internal class CF431_D2_A
+    {
+        //Black Square https://codeforces.com/contest/431/problem/A
+        public void sol()
+        {
+            int[] stripsCalories = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] sequence = Console.ReadLine().Select(c => int.Parse(c.ToString())).ToArray();
+            int totalCalories = 0;
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                int calory = sequence[i];
+                totalCalories += stripsCalories[calory - 1];
+            }
+            Console.WriteLine(totalCalories);
+        }
+    }
+}
