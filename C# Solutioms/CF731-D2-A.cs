536ec54e3593f36b85341d2508d2d0291d68f4a8
Manal ELSAYED <manal.elsayed@intalio.com>
2024-02-13 22:52:25 +0200

    CF731-D2-A Night at the Museum

---
+++ C# Solutioms/CF731-D2-A.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.C__Solutioms
+{
+    internal class CF731_D2_A
+    {
+        //https://codeforces.com/contest/731/problem/A Night at the Museum
+        public void sol() {
+            string s = Console.ReadLine();
+            int rotations = 0;
+            char currentStop = 'a';
+            foreach (char c in s)
+            {
+                if ((Math.Abs(c - currentStop)) > (26 - Math.Abs(c - currentStop)))
+                {
+                    rotations += (26 - Math.Abs(c - currentStop));
+                }
+                else
+                {
+                    rotations += Math.Abs(c - currentStop);
+                }
+                currentStop = c;
+            }
+            Console.WriteLine(rotations.ToString());
+
+
+        }
+    }
+}
